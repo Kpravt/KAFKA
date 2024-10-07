909bab3e34344e47c4a3f49545439d5b12659fa1
Praveen_k_Tiwary <pk0099780@gmail.com>
2024-10-08 00:31:06 +0530

    Task-004:Send Csorrelation ID

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,7 @@
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.HexFormat;
 
 public class Main {
   public static void main(String[] args){
@@ -19,6 +20,15 @@ public class Main {
        serverSocket.setReuseAddress(true);
        // Wait for connection from client.
        clientSocket = serverSocket.accept();
+       /*
+        * In the updated code, a string outputString is created to represent the hardcoded response that needs to be sent to the client. This string contains the hexadecimal representation of the message length (which is not validated at this stage and is set to 00000000) followed by the hardcoded correlation ID 00000007.
+The HexFormat class is then used to convert this hexadecimal string into a byte array output. The HexFormat.of().parseHex(outputString) method parses the hexadecimal string into a byte array that can be sent over the network.
+Finally, the output byte array is written to the client's output stream using clientSocket.getOutputStream().write(output), and clientSocket.getOutputStream().flush() is called to ensure that the data is sent immediately and not buffered.
+        */
+       String outputString = "0000000000000007";
+       byte[] output = HexFormat.of().parseHex(outputString);
+       clientSocket.getOutputStream().write(output);
+       clientSocket.getOutputStream().flush();
      } catch (IOException e) {
        System.out.println("IOException: " + e.getMessage());
      } finally {
